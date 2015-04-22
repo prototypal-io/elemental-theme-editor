6c9d966d519bbfb7523f54876e8a5e3476b748a2
James Fickel <jamesfickel@gmail.com>
2015-04-22 02:13:47 -0700

    demo working

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,40 +2,33 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   init: function() {
-    var self = this;
-    Ember.$.ajax({
-      url: 'http://localhost:4200/theme',
-      type: 'GET',
-      success: function(data) {
-         var themeData = JSON.parse(data);
-         self.setProperties(themeData);
-       }
+    Ember.$.getJSON('http://localhost:4200/theme').then(json => {
+      this.setProperties(json);
     });
   },
 
-  fontFamily: 'Arial',
-  scale: '2:1',
-  color: '#304FFE',
+  fontFamily: null,
+  scale: null,
+  color: null,
 
   actions: {
-    edit: function() {
-      // var jsonData = JSON.stringify({
-      //   fontFamily: this.fontFamily,
-      //   scale: this.scale,
-      //   color: this.color
-      // });
+    reload() {
+      window.location.reload();
+    },
 
-      var jsonData = JSON.stringify({ color: '#111122', fontFamily: 'Arial', scale: '2:1' });
+    edit() {
+      console.log('editing')
+      let data = this.getProperties('fontFamily', 'scale', 'color');
 
-
-      Ember.$.ajax({ url: 'http://localhost:4200/theme',
-                     type: 'POST',
-                     contentType: 'application/json',
-                     data: jsonData,
-                     success: function(data) {
-                       debugger;
-                     }
-                  });
+      Ember.$.ajax('http://localhost:4200/theme', {
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data)
+      }).then(json => {
+        console.log('success');
+      }, xhr => {
+        console.log('failure');
+      });
     }
   }
 });
